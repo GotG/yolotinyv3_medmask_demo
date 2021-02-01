9bacbd48c65ea85c179ff6b9a36e4a9d99f5b7f9
GotG <61444721+GotG@users.noreply.github.com>
2021-02-01 14:30:26 +0200

    update

--- folder2textYolo.py
+++ folder2textYolo.py
@@ -3,22 +3,17 @@ import argparse
 import random
 from collections import Counter
 parser = argparse.ArgumentParser()
-parser.add_argument('test_pct',help="percentage of images to be used for testing, the rest are used for training",
+parser.add_argument('train_pct',help="percentage of images to be used for training, the rest are used for validation and testing",
+                    type=int)
+parser.add_argument('valid_pct',help="percentage of images to be used for validation, the rest are used for training and testing",
                     type=int)
 parser.add_argument('yolodataset',help="path to directory with images and yolo annotations",
                     type=str)
 args = parser.parse_args()
-#dataset_path = '/home/leeroy/Documents/Datasets/yolotinyv3_medmask_demo/obj'
-
-# Percentage of images to be used for the test set
-
 
 
 counter = 1  
 
-
-testfiles=[]
-trainfiles=[]
 extensions = []
 images=[]
 #check extension of files in folder:
@@ -38,9 +33,12 @@ for filename in os.listdir(args.yolodataset):
 
 number_of_images = len(images)
    
-index_test = round(number_of_images*args.test_pct/100)
-testfiles = random.sample(images,index_test)
-trainfiles = list(set(images).difference(set(testfiles)))
+index_valid = round(number_of_images*args.valid_pct/100)
+validfiles = random.sample(images,index_valid)
+traintestfiles = list(set(images).difference(set(validfiles)))
+index_train = round(len(traintestfiles)*(args.train_pct/(100-args.valid_pct)))
+trainfiles = random.sample(traintestfiles,index_train)
+testfiles = list(set(traintestfiles).difference(set(trainfiles)))
 
 print('Number of images:',number_of_images)
 
@@ -49,8 +47,12 @@ with open('train.txt', mode='w') as f:
     for item in trainfiles:
         f.write(item + "\n")
 
+with open('valid.txt', mode='w') as f:
+    for item in validfiles:
+        f.write(item + "\n")
 with open('test.txt', mode='w') as f:
     for item in testfiles:
         f.write(item + "\n")
 print('Number of images used for training',str(len(trainfiles)))
+print('Number of images used for validation',str(len(validfiles)))
 print('Number of images used for testing',str(len(testfiles)))
